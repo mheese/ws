0809233cd1a2cb13d597fcdb5451b151dfb1c960
Marcus Heese <marcus@fintechdev.net>
2016-11-17 12:28:36 -0800

    first https version

--- ws.go
+++ ws.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"os"
@@ -26,6 +27,10 @@ func main() {
 			fmt.Fprintf(os.Stderr, "  %s\n", os.Args[0])
 			fmt.Fprintf(os.Stderr, "  %s /var/www\n", os.Args[0])
 			fmt.Fprintf(os.Stderr, "  %s /var/www :8080\n\n", os.Args[0])
+			fmt.Fprintf(os.Stderr, "Environment Variables:\n\n")
+			fmt.Fprintf(os.Stderr, "  TLS_CERT_PATH:  if set, path to PEM encoded certificate chain\n")
+			fmt.Fprintf(os.Stderr, "  TLS_KEY_PATH:   if set, path to PEM encoded key\n\n")
+			fmt.Fprintf(os.Stderr, "Note: if either of the variables are set, this becomes an HTTPS server.\n\n")
 			os.Exit(2)
 		}
 	}
@@ -49,10 +54,42 @@ func main() {
 
 	}
 
-	fmt.Printf("Starting webserver with file root at '%s', with ListenAndServe at '%s'\n", path, port)
-	http.Handle("/", Log(http.StripPrefix("/", http.FileServer(http.Dir(path)))))
-	if err = http.ListenAndServe(port, nil); err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
-		os.Exit(1)
+	// get environment variables and see if we want to use TLS
+	useTLS := false
+	tlsCertFile := os.Getenv("TLS_CERT_PATH")
+	tlsKeyFile := os.Getenv("TLS_KEY_PATH")
+	if len(tlsCertFile) > 0 && len(tlsKeyFile) > 0 {
+		useTLS = true
+	}
+
+	// https
+	if useTLS {
+		tlsCfg := tls.Config{
+			MinVersion: tls.VersionTLS12,
+			MaxVersion: tls.VersionTLS12,
+		}
+
+		mux := http.NewServeMux()
+		mux.Handle("/", Log(http.StripPrefix("/", http.FileServer(http.Dir(path)))))
+
+		s := &http.Server{
+			Addr:      port,
+			TLSConfig: &tlsCfg,
+			Handler:   mux,
+		}
+
+		fmt.Printf("Starting HTTPS webserver with file root at '%s', with ListenAndServe at '%s'\n", path, port)
+		if err = s.ListenAndServeTLS(tlsCertFile, tlsKeyFile); err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+			os.Exit(1)
+		}
+		// http
+	} else {
+		http.Handle("/", Log(http.StripPrefix("/", http.FileServer(http.Dir(path)))))
+		fmt.Printf("Starting HTTP webserver with file root at '%s', with ListenAndServe at '%s'\n", path, port)
+		if err = http.ListenAndServe(port, nil); err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+			os.Exit(1)
+		}
 	}
 }
